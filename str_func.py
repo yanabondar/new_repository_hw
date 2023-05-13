6a254b6590c099d8f1a5d94c49b5a2b4d5129613
yanabondar <132399852+yanabondar@users.noreply.github.com>
2023-05-13 14:01:32 +0400

    Update str_func.py added docstring

--- str_func.py
+++ str_func.py
@@ -1,5 +1,6 @@
-string = input()
 def upperer():
+    """Функция получает строку и возвращает ее заглавными буквами"""
+    string = input()
     upper_string = string.upper()
     print(upper_string)
 
